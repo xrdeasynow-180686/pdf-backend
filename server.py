c29e8a12bd465069bd25fbf677bc5343392da46e
Harman <xrdeasynow@gmail.com>
2026-04-05 08:35:44 +0930

    fix merge conflict in server.py

--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from pypdf import PdfMerger
 import os
 import uuid
+
+from pypdf import PdfReader, PdfWriter
 
 app = Flask(__name__)
 CORS(app)
@@ -15,7 +16,7 @@ os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 @app.route("/")
 def home():
-    return {"message": "PDF Merge API is running"}
+    return {"message": "PDF Merge API Running"}
 
 @app.route("/merge", methods=["POST"])
 def merge_pdfs():
@@ -24,24 +25,25 @@ def merge_pdfs():
     if len(files) < 2:
         return {"error": "Need at least 2 PDFs"}, 400
 
-    merger = PdfMerger()
-
-    saved_files = []
+    writer = PdfWriter()
 
     for file in files:
         filename = str(uuid.uuid4()) + ".pdf"
         path = os.path.join(UPLOAD_FOLDER, filename)
         file.save(path)
-        saved_files.append(path)
-        merger.append(path)
+
+        reader = PdfReader(path)
+
+        for page in reader.pages:
+            writer.add_page(page)
 
     output_file = os.path.join(
         OUTPUT_FOLDER,
         f"merged_{uuid.uuid4()}.pdf"
     )
 
-    merger.write(output_file)
-    merger.close()
+    with open(output_file, "wb") as f:
+        writer.write(f)
 
     return send_file(output_file, as_attachment=True)
 
